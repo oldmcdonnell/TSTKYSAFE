45892512c77e71f774bb3d9ce86b59268675f8e4
Eoin McDonnell <oldmcdonnell@gmail.com>
2025-06-19 00:00:24 -0400

    login or regist component

---
+++ frontend/src/components/LoginorRegister.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white shadow-lg p-8 rounded-lg w-full max-w-md">
+      <div class="flex justify-around mb-4">
+        <button @click="showLogin = true" class="font-bold" :class="{ 'text-blue-500': showLogin }">Login</button>
+        <button @click="showLogin = false" class="font-bold" :class="{ 'text-blue-500': !showLogin }">Register</button>
+      </div>
+      <LoginForm v-if="showLogin" />
+      <RegisterForm v-else />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from './LoginForm.vue'
+import RegisterForm from './RegisterForm.vue'
+
+export default {
+  name: 'LoginOrRegister',
+  components: { LoginForm, RegisterForm },
+  data() {
+    return {
+      showLogin: true,
+    }
+  },
+}
+</script>
